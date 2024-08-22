912d59c393c2447ecdb26c249c73224122e7a583
frank Tchaa <paultchaa@gmail.com>
2024-08-22 21:14:03 +0100

    bd mongodb atlas updated

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,8 @@ const packageRoutes = require('./routes/package')
 const app = express()
 
 // connexion mongoDB..
-MONGO_URL='mongodb://localhost:27017/gozem'
+// MONGO_URL='mongodb://localhost:27017/gozem'
+MONGO_URL='mongodb+srv://franky:franky@cluster0.jarxe.mongodb.net/gozem?retryWrites=true&w=majority'
 
 mongoose.connect(MONGO_URL)
     .then(() => console.log('Connexion OK'))
